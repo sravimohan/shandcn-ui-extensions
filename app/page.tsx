70e58f2e4483ab73a5c3e00b5d02563e1297c46b
Ravi Mohan <ravi.mohan@roonga.com.au>
2023-10-19 17:41:09 +1100

    ci : doc

--- app/page.tsx
+++ app/page.tsx
@@ -14,9 +14,17 @@ export default function Home () {
   const postTextAreaId = useId()
   const [value, setValue] = useState("hello world ");
 
+  const sample = `<TextareaAutoHeight
+    id={postTextAreaId}
+    name="sample"
+    value={value}
+    onChange={(e) => setValue(e.target.value)}
+    className="w-96"
+    placeholder="Write your post here..." />`
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="flex flex-col space-y-2">
+    <main className="flex min-h-screen flex-col items-center prose justify-between p-24">
+      <div className="flex flex-col space-y-6">
         <label htmlFor={postTextAreaId}>
           Try typing a long message
         </label>
@@ -28,6 +36,14 @@ export default function Home () {
           className="w-96"
           placeholder="Write your post here..."
         />
+        <div className="mt-12">
+          <blockquote><p>
+            Usage: Copy <a href="https://github.com/sravimohan/shandcn-ui-extensions/blob/9b4ea630abac18695bd56c87a141dbf13b4fbf1d/components/ui/textarea-auto-height.tsx#L1C14-L1C14">component source</a> to your project.
+          </p></blockquote>
+          <div className="relative rounded-lg w-full bg-muted px-6 py-4 font-mono text-sm font-semibold">
+            <pre>{sample}</pre>
+          </div>
+        </div>
       </div>
     </main>
   );
